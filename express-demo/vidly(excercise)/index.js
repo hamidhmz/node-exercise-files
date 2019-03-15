e92bd721f1c93ee634c7dc0265b2732d01da9e3f
hamidreza nasrollahy <h.mosaferkocholo@gmail.com>
2019-03-15 20:20:22 +0330

    mongoDB is new topic

--- express-demo/vidly(excercise)/index.js
+++ express-demo/vidly(excercise)/index.js
@@ -1,64 +1,163 @@
+const mongoose = require("mongoose");
 const Joi = require("joi");
 const express = require('express');
 const app = express();
-
+mongoose.connect("mongodb://localhost/genre")
+    .then(() => console.log("Connected to MongoDB..."))
+    .catch(err => console.error("could not connect to mongoDB...",err));
 app.use(express.json());
-
-let genres = [
-    {id:1,name:"Drama"},
-    {id:2,name:"Romance"},
-    {id:3,name:"Music"},
-    {id:3,name:"Comedy"},
-    {id:3,name:"Western"},
-    {id:3,name:"Horror"},
-    {id:3,name:"Animation"},
-    {id:3,name:"Fantasy"},
-];
+const genreSchema = new mongoose.Schema({
+    name: {
+        type:String,
+        required:true,
+        minlength:3,
+        maxlength:40,
+        uppercase:true,
+        unique:true
+    }
+});
+const Genre = mongoose.model("Genre",genreSchema);
+const customersSchema = new mongoose.Schema({
+    isGold:{
+        type:Boolean,
+        required:true
+    },
+    name:{
+        type:String,
+        required:true
+    },
+    phone:{
+        type:String,
+        required:true,
+        unique:true
+    }
+});
+const Customer = mongoose.model("Customer",customersSchema);
+async function createCustomer(isGold,name,phone){
+    const customer = new Customer({
+        isGold: isGold,
+        name:name,
+        phone:phone
+    });
+    try{
+        // await course.validate(); you can manually validate
+        const result = await customer.save();
+        console.log(result);
+    }catch(ex){
+        for(field in ex.errors)
+            console.log(ex.errors[field].message);
+    }
+}
+createCustomer(false,"Customer 1","12345");
+async function createCourse(){
+    const genre = new Genre({
+        name: "Drama"
+    });
+    try{
+        // await course.validate(); you can manually validate
+        const result = await genre.save();
+        console.log(result);
+    }catch(ex){
+        for(field in ex.errors)
+            console.log(ex.errors[field].message);
+    }
+}
+// createCourse();
+// let genres = [
+//     {id:1,name:"Drama"},
+//     {id:2,name:"Romance"},
+//     {id:3,name:"Music"},
+//     {id:3,name:"Comedy"},
+//     {id:3,name:"Western"},
+//     {id:3,name:"Horror"},
+//     {id:3,name:"Animation"},
+//     {id:3,name:"Fantasy"},
+// ];
 
 app.get("/",(req,res)=>{
     res.send("Wellcome to nodemon its a movie genre webservices! <br> for see all genres you can go to <a href='/api/genres'>this address</a>");
 });
-app.get("/api/genres",(req,res)=>{
+app.get("/api/genres",async (req,res)=>{
+    const genres = await Genre
+    .find()
+    .select({name:1});
     res.send(genres);
 });
-app.get("/api/genre/:id",(req,res)=>{
-    let genre = genres.find(c => c.id === parseInt(req.params.id));
-    res.send(genre);
+app.get("/api/genre/:id",async (req,res)=>{
+    const genres = await Genre
+        .findById(req.params.id);
+    // let genre = genres.find(c => c.id === parseInt(req.params.id));
+    res.send(genres);
 });
-app.post("/api/genres",(req,res)=>{
+app.post("/api/genres",async (req,res)=>{
     let { error } = validateCourse(req.body);
     if (error){
         return res.status(400).send(error.details[0].message);
     }
-    let genre = { id: genres.length + 1 ,name: req.body.name};
-    genres.push(genre);
-    res.send(genre);
+    const genre = new Genre({
+        name:req.body.name
+    });
+    try{
+        const result = await genre.save();
+        res.send(result);
+    }catch(ex){
+        for(field in ex.errors)
+            console.log(ex.errors[field].message);
+
+        res.send(ex.errmsg);
+    }
+    // let genre = { id: genres.length + 1 ,name: req.body.name};
+    // genres.push(genre);
+    // res.send(genre);
+});
+app.put("/api/genre/:id",async (req,res)=>{
+    try{
+        const result = await Genre.update({ _id: req.params.id },{
+            $set:{
+                name:req.body.name,
+            }
+        }); 
+    // console.log(result);
     
-});
-app.put("/api/genre/:id",(req,res)=>{
-    let genre = genres.find(c => c.id === parseInt(req.params.id));
-    if(!genre){
-        return res.status(404).send("the genre with the given ID was not available");
-    }else{
-        let { error } = validateCourse(req.body);
-        if (error){
-            return res.status(400).send(error.details[0].message);
-        }
-        genre.name = req.body.name;
-        
-        res.send(genre);
+        res.send(result);
+    }catch(ex){
+        for(field in ex.errors)
+            console.log(ex.errors[field].message);
+
+        res.send(ex.errmsg);
     }
+    // let genre = genres.find(c => c.id === parseInt(req.params.id));
+    // if(!result){
+    //     // return res.status(404).send("the genre with the given ID was not available");
+    // }else{
+    //     let { error } = validateCourse(req.body);
+    //     if (error){
+    //         return res.status(400).send(error.details[0].message);
+    //     }
+    //     // genre.name = req.body.name;
+        
+    //     res.send(genre);
+    // }
 });
-app.delete("/api/genre/:id",(req,res)=>{
-    let genre = genres.find(c => c.id === parseInt(req.params.id));
-    if(!genre){
+app.delete("/api/genre/:id",async (req,res)=>{
+    try{
+        const result = await Genre.deleteOne({_id:req.params.id});
+        res.send(result);
+    }catch(ex){
+        for(field in ex.errors)
+            console.log(ex.errors[field].message);
+
         res.status(404).send("the genre with the given ID was not available");
-    }else{
-        let genreIndex = genres.indexOf(genre);
-        genres.splice(genreIndex,1);
-        
-        res.send(genre.name + " has been deleted");
     }
+    // let genre = genres.find(c => c.id === parseInt(req.params.id));
+    // if(!genre){
+    //     res.status(404).send("the genre with the given ID was not available");
+    // }else{
+    //     let genreIndex = genres.indexOf(genre);
+    //     genres.splice(genreIndex,1);
+        
+    //     res.send(genre.name + " has been deleted");
+    // }
 });
 function validateCourse(genre){
     let schema = {
--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -4,11 +4,55 @@ mongoose.connect("mongodb://localhost/playground")
     .catch(err => console.error("could not connect to mongoDB...",err));
 
 const courseSchema = new mongoose.Schema({
-    name: String,
+    // name: String,
+    name: {
+        type:String,
+        required:true,
+        minlength:5,
+        maxlength:255,
+        uppercase:true,
+        unique:true
+        // match:/pattern/
+    },
+    category:{
+        type:String,
+        required:true,
+        lowercase:true,//mongo automatically change uppercase to lowercase 
+        trim:true,
+        enum:["web","mobile","network"]//this line code says category must be web or mobile or network
+    },
     author: String,
-    tags: [ String ],
+    // tags: [ String ],//we cannot give require to this because doesn't work we must to set custom validators 
+    tags:{
+        type: Array,
+        // validate:{
+        //     validator:function(v){
+        //         return v && v.length > 0;
+        //     },
+        //     message: "A course should have at least one tag." 
+        // },
+        //async validator 
+        validate:{
+            isAsync:true,
+            validator:function(v,callback){
+                setTimeout(()=>{
+                    const result = v && v.length > 0;
+                    callback(result);
+                },4000);
+            },
+            message: "A course should have at least one tag." 
+        },
+    },
     data: { type: Date, default: Date.now },
-    isPublished: Boolean
+    isPublished: Boolean,
+    price:{
+        type:Number,
+        min:10,
+        max:200,
+        get:v=> Math.round(v),//when read a value of property from database
+        set:v=> Math.round(v),//when set a value of property to database
+        required: function(){return this.isPublished}
+    }
 });// Schema types: String Number Date Buffer Boolean ObjectID Array
 const Course = mongoose.model("Course",courseSchema);
 
@@ -17,12 +61,22 @@ const Course = mongoose.model("Course",courseSchema);
 async function createCourse(){
     const course = new Course({
         name: "Angular Course",
+        category:"network",
         author: "Mosh",
         tags: ["Angular", "frontend"],
-        isPublished: true
+        isPublished: true,
+        price:15
     });
-    const result = await course.save();
-    console.log(result);
+    //this is mongodb validation 
+    try{
+        // await course.validate(); you can manually validate
+        const result = await course.save();
+        console.log(result);
+    }catch(ex){
+        for(field in ex.errors)
+            console.log(ex.errors[field].message);
+    }
+    
 }
 
 createCourse(); 
@@ -114,7 +168,7 @@ async function updateCourse(id){
     // update directly 
     // optionally:get the updated document
 
-    const course = await Course.findById({ _id: id },{
+    const result = await Course.update({ _id: id },{
         // mongodb update operators: https://docs.mongodb.com/manual/reference/operator/update/
         $set:{
             author:"mosh",
@@ -126,7 +180,7 @@ async function updateCourse(id){
     console.log(result);
     
 }
-createCourse("5a68fde3f09ad7646ddec17e");
+updateCourse("5a68fde3f09ad7646ddec17e");
 
 async function removeCourse(id){
     const result = await Course.deleteOne({_id:id});
@@ -135,4 +189,4 @@ async function removeCourse(id){
     const result = await Course.findByIdAndRemove({isPublished: true});
     console.log(result); 
 }
-createCourse("5a68fde3f09ad7646ddec17e");
+removeCourse("5a68fde3f09ad7646ddec17e");
